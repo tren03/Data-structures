3e4c2778cdbaa2b2c003ab26652b1671fdee8191
tren03 <82367813+tren03@users.noreply.github.com>
2023-12-15 12:13:50 +0530

    Update Nos_of_subseqence.cpp

--- Recursion/Nos_of_subseqence.cpp
+++ Recursion/Nos_of_subseqence.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
 #include<vector>
 using namespace std;
-int f(int i,vector<int> a,vector<int> arr,int n,int s)
+int f(int i,vector<int> a,vector<int> arr,int n)
 {
     if(i>=n)
     {
         return 1;
     }
     a.push_back(arr[i]);
-    s+=arr[i];
-    int l = f(i+1,a,arr,n,s);
+    int l = f(i+1,a,arr,n);
     a.pop_back();
-    s-=arr[i];
-    int r = f(i+1,a,arr,n,s);
+    int r = f(i+1,a,arr,n);
     return l+r;
 }
 
 int main() {
     vector<int> arr{1,2,1};
     int n = arr.size();
-    int s;
     vector<int> a;
-    cout<<f(0,a,arr,n,s);
+    cout<<f(0,a,arr,n);
     return 0;
 }
